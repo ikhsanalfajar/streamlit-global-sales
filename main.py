e92cd73af901e9ec6ffb6cd04a708362f679590a
ikhsanalfajar <116376200+ikhsanalfajar@users.noreply.github.com>
2023-10-21 23:38:23 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_global_sales_vidio_game.sav', 'rb'))
+
+st.title('Estimasi Data Global Sales Vidio Game')
+
+NA_Sales = st.number_input('Input Penjualan Amerika Utara')
+EU_Sales = st.number_input('Input Penjualan Eropa')
+JP_Sales = st.number_input('Input Penjualan Jepang')
+Other_Sales = st.number_input('Input Penjualan Lainnya')
+
+predict = ''
+
+if st.button('Estimasi Global'):
+    predict = model.predict(
+        [[NA_Sales, EU_Sales, JP_Sales, Other_Sales]]
+    )
+    st.write('Estimasi Penjualan Global : ', predict)
